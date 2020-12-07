54d14f4e18a51439a9a859ace204cb5a0019b7b1
Ivan-luxoft <igubanov@luxoft.com>
2020-12-07 15:16:37 +0300

    implement method Sum for two or less number in argument

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -1,6 +1,7 @@
 ﻿// NUnit 3 tests
 // See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
 using System;
+using System.Linq;
 
 namespace TDDKata
 {
@@ -8,7 +9,41 @@ namespace TDDKata
     {
         internal int Sum(string v)
         {
-            throw new NotImplementedException();
+            var arrayOfInteger = SplitStringToArrayInt(v);
+
+            if (arrayOfInteger == null
+                || arrayOfInteger.Length > 2)
+                return -1;
+
+            return arrayOfInteger.Aggregate((i1, i2) => i1 + i2);
+        }
+
+        private int[] SplitStringToArrayInt(string arg)
+        {
+            if (arg == null)
+                return null;
+
+            if (arg == "")
+                return new int[] { 0 };
+
+            var result = default(int[]);
+            var splitedArguments = arg.Split(',');
+
+            if (splitedArguments.Any(s => s.Contains("-")))
+                return result;
+
+            try
+            {
+                result = splitedArguments
+                    .Select(s => int.Parse(s))
+                    .ToArray();
+            }
+            catch
+            {
+                return result;
+            }
+
+            return result;
         }
     }
 }
